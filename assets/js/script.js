06d6ac5eb7c278e40d3f516fde010838864d7458
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-12-24 17:08:15 +0530

    modified:   About_us.html 	modified:   Acting.html 	modified:   Campuses.html 	modified:   Cinematography.html 	modified:   Colour_grading.html 	modified:   Contact.html 	modified:   Direction.html 	modified:   Dubbing.html 	modified:   Editing.html 	modified:   Photography.html 	modified:   Script_writing.html 	modified:   Sound_designing.html 	modified:   Tv_news_reading_and_anchoring.html 	new file:   assets/css/homestyle.css 	new file:   assets/css/style_home.css 	new file:   assets/images/home_s1/actingcourse.jpg 	new file:   assets/images/home_s1/cinematographycourse.jpg 	new file:   assets/images/home_s1/editingcourse.jpg 	new file:   assets/images/home_s1/photographycourse.jpg 	new file:   assets/images/home_s1/scriptwritingcourse.jpg 	modified:   assets/js/script.js 	modified:   index.html

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 1000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+let frame = document.getElementById("my_frame");
+window.addEventListener("resize", hightcalc);
+
+function hightcalc() {
+    var h = window.innerHeight;
+    console.log(h);
+    var temp1 = document.getElementById("nav_height").offsetHeight;
+    console.log(temp1);
+    let total_height;
+    if(window.innerWidth > 993){
+        total_height = h - temp1;
+    }
+    else{
+        total_height = h;
+    }
+    console.log(total_height);
+    frame.style.height = total_height + "px";
+}
+hightcalc();
